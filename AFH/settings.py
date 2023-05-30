b5491d4386a3ed12a0e94b13fa8aec4354ae161d
Aakash <122861914+Aakasha063@users.noreply.github.com>
2023-05-30 11:49:17 +0530

    Update settings.py

--- AFH/settings.py
+++ AFH/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8kedrn0%ra0vilqzib=t+j(@pc0g+d9wtw)_+-qia+-otuxmo8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,9 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'eCommerce/static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' ,'static')
+STATIC_ROOT = os.path.join(BASE_DIR ,'static')
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
